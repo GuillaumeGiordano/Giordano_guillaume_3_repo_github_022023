dfe5d04e8743e6651f46d8c5f53afd0a969e09b7
GuillaumeGiordano <guigoo.giordano@gmail.com>
2023-05-10 16:36:14 +0200

    Issue: test
    MAJ correction divers
    
    Etat: EC

--- FrontEnd/js/fonctions/connexion.js
+++ FrontEnd/js/fonctions/connexion.js
@@ -7,77 +7,72 @@ import {
 } from "./dom.js";
 
 
-/**
- * 
- */
+
 export function setLogin() {
+    document.querySelector("#formLogin").addEventListener("submit", getConnexion);
+};
 
-    // Select the form
-    const formLogin = document.querySelector("#formLogin");
-    formLogin.addEventListener("submit", async function (event) {
 
-        // stop form method
-        event.preventDefault();
+async function getConnexion(event) {
+    // stop form method
+    event.preventDefault();
 
-        // Init messageError
-        removeErrorMessage("main");
+    // Init messageError
+    removeErrorMessage("main");
 
-        // Delete error Style
-        removeErrorStyle("#mail");
-        removeErrorStyle("#pass");
+    // Delete error Style
+    removeErrorStyle("#mail");
+    removeErrorStyle("#pass");
 
-        // control email
-        const email = event.target.querySelector("#mail").value;
-        const pattern = /^[a-z0-9.-]{2,}@+[a-z0-9.-]{2,}$/i;
-        if (email === "") {
-            addErrorStyle("#mail");
-            return;
-        };
-        if (!pattern.test(email)) {
-            alert("Ceci n'est pas une adresse e-mail");
-            addErrorStyle("#mail");
-            return;
-        };
+    // control email
+    const email = event.target.querySelector("#mail").value;
+    const pattern = /^[a-z0-9.-]{2,}@+[a-z0-9.-]{2,}$/i;
+    if (email === "") {
+        addErrorStyle("#mail");
+        return;
+    };
+    if (!pattern.test(email)) {
+        alert("Ceci n'est pas une adresse e-mail");
+        addErrorStyle("#mail");
+        return;
+    };
 
-        // control password
-        const passWord = event.target.querySelector("#pass").value;
-        if (passWord === "") {
-            addErrorStyle("#pass");
-            return;
-        };
+    // control password
+    const passWord = event.target.querySelector("#pass").value;
+    if (passWord === "") {
+        addErrorStyle("#pass");
+        return;
+    };
 
-        // Get object full valid after pass controls
-        const user = {
-            "email": email,
-            "password": passWord,
-        };
+    // Get object full valid after pass controls
+    const user = {
+        "email": email,
+        "password": passWord,
+    };
 
-        // Connect to API
-        const cnx = await fetch('http://localhost:5678/api/users/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(user)
-        });
-
-        // Controls connection
-        const r = cnx.status;
-        if (r === 401 || r === 404) {
-            addErrorMessage("Il y a une erreur sur les identifiants !", "main");
-            document.querySelector("#mail").value = "";
-            document.querySelector("#pass").value = "";
-            return;
-        };
-
-        // Connection OK => Next
-        if (cnx.ok && r === 200) {
-            const data = await cnx.json();
-            localStorage.setItem("SESSION", data.token);
-            window.location.href = './index.html';
-            alert("Bienvenue " + email);
-        };
-
+    // Connect to API
+    const cnx = await fetch('http://localhost:5678/api/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
     });
 
-};
+    // Controls connection
+    const r = cnx.status;
+    if (r === 401 || r === 404) {
+        addErrorMessage("Il y a une erreur sur les identifiants !", "main");
+        document.querySelector("#mail").value = "";
+        document.querySelector("#pass").value = "";
+        return;
+    };
+
+    // Connection OK => Next
+    if (cnx.ok && r === 200) {
+        const data = await cnx.json();
+        localStorage.setItem("SESSION", data.token);
+        window.location.href = './index.html';
+        alert("Bienvenue " + email);
+    };
+}
--- FrontEnd/js/fonctions/navigation.js
+++ FrontEnd/js/fonctions/navigation.js
@@ -8,27 +8,31 @@ export function renderNav() {
     const links = document.querySelectorAll(".nav__li");
 
     links.forEach(item => {
-        console.log(item);
-        item.addEventListener("click", classModifierOnClick)
+        // console.log(item);
+        item.addEventListener("click", menuItemBold)
     });
 
 };
 
 
-const classModifierOnClick = function (e) {
-    e.preventDefault();
+function menuItemBold(e) {
+    // e.preventDefault();
 
-    // console.log(hrefLink);
+    // Clear
     document.querySelectorAll(".nav__li").forEach(link => {
-        console.log(link);
         link.classList.remove("nav__li--select");
     });
 
-    // console.log(e.target);
-    // razClassNav();
 
     const hrefLink = e.target.getAttribute("href");
-    window.location.href = hrefLink;
+
+
     this.classList.add("nav__li--select");
+
+
+
+    // window.location.href = hrefLink;
+
+
 };
 
